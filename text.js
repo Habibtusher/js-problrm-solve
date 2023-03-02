4e6f481c378e11ee74ed30a3b066918a926fb2c8
Habibtusher <tusherhrt@gmail.com>
2023-03-02 09:40:24 +0600

    first commit

---
+++ text.js
@@ -0,0 +1,40 @@
+const result = (num) => {
+    let arr = []
+      for (let i = 1; i <= num; i++) {
+        if(i%3 ===0 && i%5 === 0){
+          arr.push("fizzbazz")
+        }
+        else if(i%3 === 0){
+            arr.push("fizz") 
+        
+        }
+        else if(i%5 === 0){
+           arr.push("bazz") 
+       
+        }
+        else{
+           arr.push(i.toString())
+        }
+      }
+      return arr;
+  }
+  console.log(result(15))
+
+
+  const  merge=(a, b, i=0)=> {
+    return a.slice(0, i).concat(b, a.slice(i));
+  }
+  
+  const newRes = merge([1, 3], [2, 4], 1);
+  console.log(newRes);
+
+
+  const getArrayLength = (arr) => {
+    let length;
+    const flatArray = arr.flat(Infinity);
+    if (arr.length === 0) {
+      length = 0;
+    } else length = flatArray.length;
+    return length;
+  };
+  console.log(getArrayLength([1, [2, [3, [4,[5,[67,[43]]]]]]]));
